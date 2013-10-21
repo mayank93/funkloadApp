425408c7b9efe5347207d4c206d10ea38ccec337
Ankush Jain <anku.94@gmail.com>
2013-10-22 00:43:46 +0530

    Added grabber

---
+++ grabber.py
@@ -0,0 +1,23 @@
+import urllib2
+import re
+
+def grabber(url):
+    """ Takes a URL string as input, returns a list containing all URLs in that site """
+    if not url.startswith('http'):
+        url = 'http://' + url
+    handle = urllib2.urlopen(url)
+    data = handle.read()
+    r = re.findall('href=\"(.*?)\"', data)
+    for i in r:
+        if i is None:
+            pass
+        if not i.startswith('http'):
+            if not i.startswith('/'):
+                i = '/' + i
+            i = url + i
+            print i
+
+if __name__ == "__main__":
+    TEST_URL = "intranet.iiit.ac.in"
+    output = grabber(TEST_URL)
+    print output
